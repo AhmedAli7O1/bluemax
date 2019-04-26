24ca5174789f37aada4eaf60c8297247d61048cd
Ahmed Ali <bluemax7o1@gmail.com>
2019-04-26 21:34:27 +0200

    add new response methods

--- apptest/controllers/HelloController.js
+++ apptest/controllers/HelloController.js
@@ -2,37 +2,24 @@
 
 
 function getAllHello (req, res) {
-  const msg = `Hello there\nthis is the BlueMax Node.js Webserver\nFunction: getAllHello`;
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(msg);
+  res
+    .status(200)
+    .json({ fn: "getAllHello" });
 }
 
 function getHelloByName (req, res) {
-  const helloName = req.params.helloName;
-  const msg = `Hello, ${helloName}!\nthis is the BlueMax Node.js Webserver\n`;
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(msg);
+  res
+    .header("header-one", "1")
+    .header("header-two", "2")
+    .json({ fn: "getHelloByName", params: req.params });
 }
 
 function getHelloById (req, res) {
-  const helloId = req.params.helloId;
-
-  const msg = `Hello there\nthis is the BlueMax Node.js Webserver\nFunction: getHelloById\nhelloId: ${helloId}`;
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(msg);
+  res.json({ fn: "getHelloById", params: req.params });
 }
 
 function getHelloCommentById (req, res) {
-  const helloId = req.params.helloId;
-  const commentId = req.params.commentId;
-
-  const msg = `Hello there\nthis is the BlueMax Node.js Webserver\nFunction: getAllHello\nhelloId: ${helloId} - commentId: ${commentId}`;
-
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(msg);
+  res.json({ fn: "getHelloCommentById", params: req.params });
 }
 
 
